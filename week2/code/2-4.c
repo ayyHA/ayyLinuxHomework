e9b423babcb77004593c863ae8879c49cdf0cebf
ayyHA <ayyHaibaraAi@gmail.com>
2020-10-11 10:09:52 +0800

    update 2-4.c and 2-4

--- week2/code/2-4.c
+++ week2/code/2-4.c
@@ -5,9 +5,13 @@ int main(){
 		char name[64];
 		snprintf(name,sizeof(name),"%d.txt",cnt);
 		int fd = creat(name,0644);
-		sleep(10);
+		//sleep(10);
 		cnt++;
 		close(fd);
+		if(cnt==1022){
+			printf("The file description can be closed normally~");
+			break;
+		}
 	}
 	return 0;
 }
